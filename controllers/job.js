d7655f2e29ca8da15678b1e4d473f7cf462c378a
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2022-11-12 09:35:41 +0100

    updated job controller

--- controllers/job.js
+++ controllers/job.js
@@ -5,7 +5,6 @@ const checkPerms = require("../utils/checkPerms");
 
 const createJob = async (req, res) => {
   const { company, position } = req.body;
-
   if (!company || !position) {
     throw new CustomError.BadRequestError(
       "Company and position fields cannot be empty!"
@@ -19,7 +18,6 @@ const createJob = async (req, res) => {
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find().populate({ path: "user", select: "_id name" });
-
   if (!jobs.length) {
     throw new CustomError.NotFoundError("No jobs found");
   }
@@ -33,37 +31,31 @@ const getSingleJob = async (req, res) => {
       `Coudn't find job with id ${req.params.id}`
     );
   }
-
   checkPerms(req.user, job.user);
   res.status(StatusCodes.OK).json(job);
 };
 
 const showMyJobs = async (req, res) => {
   const myJobs = await Job.find({ user: req.user.userId });
-
   if (myJobs.length < 1) {
     throw new CustomError.NotFoundError("You haven't created any jobs yet!");
   }
-
   res.status(StatusCodes.OK).json({ myJobs, count: myJobs.length });
 };
 
 const updateJob = async (req, res) => {
   const { company, position, status } = req.body;
-
   if (!company || !position) {
     throw new CustomError.BadRequestError(
       "Company and position fields cannot be empty!"
     );
   }
-
   const job = await Job.findOne({ _id: req.params.id });
   if (!job) {
     throw new CustomError.NotFoundError(
       `Coudn't find job with id ${req.params.id}`
     );
   }
-
   checkPerms(req.user, job.user);
   job.company = company;
   job.position = position;
@@ -74,13 +66,11 @@ const updateJob = async (req, res) => {
 
 const deleteJob = async (req, res) => {
   const job = await Job.findOneAndDelete({ _id: req.params.id });
-
   if (!job) {
     throw new CustomError.NotFoundError(
       `Coudn't find job with id ${req.params.id} !`
     );
   }
-
   checkPerms(req.user, job.user);
   res.status(StatusCodes.OK).json({ status: "Success!", msg: "Job removed!" });
 };
